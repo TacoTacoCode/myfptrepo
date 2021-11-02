8a0e4d223b56e464c3236c661fea45fd10db5054
Taco <truongquoclap3008@gmail.com>
2021-11-03 03:44:35 +0700

    v

---
+++ Assisgnment2_AWS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assisgnment2_AWS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Assisgnment2_AWS/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assisgnment2_AWS.Models
+{
+    [DynamoDBTable("Product_TruongQuocLap")]
+    public class Product
+    {
+        [DynamoDBHashKey("productId")]
+        public string id { get; set; }
+        [DynamoDBRangeKey("productName")]
+        public string name { get; set; }
+
+    }
+}
